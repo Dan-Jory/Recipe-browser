2f3aa5e06eff2fdf04dfa4084343bb5f562b451b
Dinoblitx <66476932+Dinoblitx@users.noreply.github.com>
2024-12-07 21:43:52 +0000

    SQL Conversion command was wrong because of Database type mismatch. Think this has fixed it but not sure. Committing for version history

--- src/main/java/org/example/RecipeRepository.java
+++ src/main/java/org/example/RecipeRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 public interface RecipeRepository extends JpaRepository<Recipe, Long>
 {
-    @Query("SELECT recipe FROM Recipe recipe WHERE recipe.Name LIKE %'ingredient%'")
-    List<Recipe> searchIngredient(@Param("ingredient") String ingredients);
+    @Query("SELECT recipe FROM Recipe recipe WHERE LOWER(recipe.Ingredients) ILIKE LOWER(CONCAT(('%'), :ingredient1, '%')) AND " +
+            "LOWER(recipe.Ingredients) ILIKE LOWER(CONCAT(('%'), :ingredient2, '%')) AND " +
+            "LOWER(recipe.Ingredients) ILIKE LOWER(CONCAT(('%'), :ingredient3, '%'))")
+
+    List<Recipe> searchIngredient(@Param("ingredient1") String ingredient1,
+    @Param("ingredient2") String ingredient2,
+    @Param("ingredient3") String ingredient3);
 
 
 }
